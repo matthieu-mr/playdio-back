0f2c440a63cce18c22cc6b5f6ab1d36b7f31500b
karantass <b.viaud5@laposte.net>
2020-06-01 14:01:00 +0200

    ajout route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require('../models/user')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
