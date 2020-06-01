4b7de7a78608fed7ee889eb50d1a2c3c7d296b4b
matthieu-mr <m.michon.rossel@gmail.com>
2020-06-01 11:14:11 +0200

    request to soundiiz

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("hello bibiche")
   res.render('index', { title: 'Express' });
 });
+
+
+
+router.get('/soudiiz', function(req, res, next) {
+
+// req exemple : https://soundiiz.com/v1/openapi/lookup/track/US43C1603405/sources?access_token=48c1c9b73c444307914965a24ffa8c40
+  let apiToken ="48c1c9b73c444307914965a24ffa8c40" // token from soundiiz
+  let isrcTrackToSearch = "US43C1603405" // 
+  
+  let urlSoundiiz ="https://soundiiz.com/v1/openapi/lookup/track/"+isrcTrackToSearch+"/sources?access_token="+apiToken
+ 
+
+  var reqSoundiz = request('GET', urlSoundiiz);
+  let repSoundiz=JSON.parse(reqSoundiz.getBody())
+  console.log(repSoundiz)
+
+    res.json({ repSoundiz });
+
+ // res.render('index', { title: 'Express' });
+});
+
+
+
+/* Add soudiiz  */
 
 module.exports = router;
